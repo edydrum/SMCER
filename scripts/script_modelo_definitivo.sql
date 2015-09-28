23f586fd6cb0a1db67212709b30dd205c4fab8ed
alemanoelsilva <alemanoelsilva@gmail.com>
2015-09-28 09:44:56 -0300

    alterações nos nomes dos campos dos modelos de dados

--- scripts/script_modelo_definitivo.sql
+++ scripts/script_modelo_definitivo.sql
@@ -1,14 +1,14 @@
 CREATE TABLE tcc_teste.Circuito (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  name CHAR(255) NOT NULL,
+  nome CHAR(255) NOT NULL,
   PRIMARY KEY(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE tcc_teste.Usuario (
   id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-  name CHAR(255) NOT NULL,
+  nome CHAR(255) NOT NULL,
   email CHAR(255) NOT NULL,
-  password  CHAR(64) NOT NULL,
+  senha  CHAR(64) NOT NULL,
   PRIMARY KEY(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -18,14 +18,14 @@ CREATE TABLE tcc_teste.Alerta (
   id_usuario_fk INTEGER UNSIGNED NOT NULL,
   potencia FLOAT NOT NULL,
   habilitar TINYINT NOT NULL,
-  name VARCHAR(255) NOT NULL,
+  nome VARCHAR(255) NOT NULL,
   PRIMARY KEY(id),
   INDEX Alerta_FK_Usuario(id_usuario_fk),
   INDEX Alerta_FK_Circuito(id_circuito_fk)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE tcc_teste.Consumo_Hora (
-  timestamp TIMESTAMP NOT NULL,
+  data_hora TIMESTAMP NOT NULL,
   id_circuito_fk INTEGER UNSIGNED NOT NULL,
   potencia FLOAT NOT NULL DEFAULT 0.0,
   PRIMARY KEY(timestamp, id_circuito_fk),
@@ -33,7 +33,7 @@ CREATE TABLE tcc_teste.Consumo_Hora (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE tcc_teste.Consumo_Hora_Fechada (
-  timestamp TIMESTAMP NOT NULL,
+  data_hora TIMESTAMP NOT NULL,
   id_circuito_fk INTEGER UNSIGNED NOT NULL,
   potencia FLOAT NOT NULL DEFAULT 0.0,
   PRIMARY KEY(timestamp, id_circuito_fk),
@@ -41,7 +41,7 @@ CREATE TABLE tcc_teste.Consumo_Hora_Fechada (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE tcc_teste.Consumo_Instantaneo (
-  time INTEGER NOT NULL,
+  hora TIME NOT NULL,
   id_circuito_fk INTEGER UNSIGNED NOT NULL,
   potencia FLOAT NOT NULL DEFAULT 0.0,
   PRIMARY KEY(time, id_circuito_fk),
